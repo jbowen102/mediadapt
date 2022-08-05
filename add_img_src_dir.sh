03eb692b2b8f1e567eb2083acd4ef6b433a8092c
Jonathan <ew15dro6k216@opayq.net>
2022-08-05 17:07:53 -0400

    Create add_img_src_dir.sh

---
+++ add_img_src_dir.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+DIR_PATH="$(realpath "${1}")"
+
+
+if [[ -d "${DIR_PATH}" ]]; then
+    if [[ -e "${DIR_PATH}" ]]; then
+        :
+    else
+        echo "Input path ${1} not valid." >&2
+        exit 2
+    fi
+elif [[ -e "${DIR_PATH}" ]]; then
+    echo "Input path ${1} not valid. Must be directory, not file." >&2
+    exit 2
+fi
+
+
+
+for file in "${DIR_PATH}"/*; do
+    if [[ -d "$file" ]]; then
+        :
+    else
+        printf "\n$file\n"
+        ./add_img_src.sh "$file" "${2}"
+    fi
+done
+# https://askubuntu.com/questions/315335/bash-command-for-each-file-in-a-folder
+# https://www.cyberciti.biz/faq/bash-loop-over-file/
